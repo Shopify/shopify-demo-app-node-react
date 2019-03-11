ef0b925600296c40fa74dd30b363086c7018f929
Marki Sveen <marki.sveen@shopify.com>
2019-03-11 15:14:28 -0400

    build your user interface with polaris starter files

---
+++ server.js
@@ -0,0 +1,46 @@
+require('isomorphic-fetch');
+const Koa = require('koa');
+const next = require('next');
+const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
+const dotenv = require('dotenv');
+const { verifyRequest } = require('@shopify/koa-shopify-auth');
+const session = require('koa-session');
+dotenv.config();
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY } = process.env;
+
+app.prepare().then(() => {
+  const server = new Koa();
+  server.use(session(server));
+  server.keys = [SHOPIFY_API_SECRET_KEY];
+
+  server.use(
+    createShopifyAuth({
+      apiKey: SHOPIFY_API_KEY,
+      secret: SHOPIFY_API_SECRET_KEY,
+      scopes: ['read_products'],
+      afterAuth(ctx) {
+        const { shop, accessToken } = ctx.session;
+    
+        ctx.redirect('/');
+      },
+    }),
+  );
+    
+  server.use(verifyRequest());
+  server.use(async (ctx) => {
+      await handle(ctx.req, ctx.res);
+      ctx.respond = false;
+      ctx.res.statusCode = 200;
+      return
+  });
+
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
