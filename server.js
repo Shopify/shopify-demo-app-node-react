b50fe0d70f267b36c2e98086bce909a47f7db2a5
Marki Sveen <marki.sveen@shopify.com>
2019-03-14 15:27:42 -0400

    listen for store events with webhooks starter files

--- server.js
+++ server.js
@@ -7,33 +7,63 @@ const { verifyRequest } = require('@shopify/koa-shopify-auth');
 const session = require('koa-session');
 dotenv.config();
 const { default: graphQLProxy } = require('@shopify/koa-shopify-graphql-proxy');
+const Router = require('koa-router');
+const processPayment = require('./server/router');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY } = process.env;
+const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY, TUNNEL_URL } = process.env;
 
 app.prepare().then(() => {
   const server = new Koa();
+  const router = new Router();
   server.use(session(server));
   server.keys = [SHOPIFY_API_SECRET_KEY];
+
+  router.get('/', processPayment);
 
   server.use(
     createShopifyAuth({
       apiKey: SHOPIFY_API_KEY,
       secret: SHOPIFY_API_SECRET_KEY,
       scopes: ['read_products', 'write_products'],
-      afterAuth(ctx) {
+      async afterAuth(ctx) {
         const { shop, accessToken } = ctx.session;
         ctx.cookies.set('shopOrigin', shop, { httpOnly: false })
-        ctx.redirect('/');
+
+        const stringifiedBillingParams = JSON.stringify({
+          recurring_application_charge: {
+            name: 'Recurring charge',
+            price: 20.01,
+            return_url: TUNNEL_URL,
+            test: true
+          }
+        })
+        const options = {
+          method: 'POST',
+          body: stringifiedBillingParams,
+          credentials: 'include',
+          headers: {
+            'X-Shopify-Access-Token': accessToken,
+            'Content-Type': 'application/json',
+          },
+        };
+
+        const confirmationURL = await fetch(
+          `https://${shop}/admin/recurring_application_charges.json`, options)
+          .then((response) => response.json())
+          .then((jsonData) => jsonData.recurring_application_charge.confirmation_url)
+          .catch((error) => console.log('error', error));
+        ctx.redirect(confirmationURL);
       },
     }),
   );
   
   server.use(graphQLProxy());
+  server.use(router.routes());
   server.use(verifyRequest());
   server.use(async (ctx) => {
       await handle(ctx.req, ctx.res);
