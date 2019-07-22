2f79141d5c8c2d634eba42de7d8ac8c2c6f82714
Katie Davis <katie.davis@shopify.com>
2019-07-22 15:21:53 -0400

    fix cookies (#36)

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import App from 'next/app';
 import Head from 'next/head';
 import { AppProvider } from '@shopify/polaris';
 import { Provider } from '@shopify/app-bridge-react';
+import Cookies from "js-cookie";
 import '@shopify/polaris/styles.css';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
@@ -13,14 +14,9 @@ const client = new ApolloClient({
 });
 
 class MyApp extends App {
-  static async getInitialProps(server) {
-    const shopOrigin = server.ctx.query.shop;
-    return { shopOrigin };
-  }
-
   render() {
-    const { Component, pageProps, shopOrigin } = this.props;
-    const config = { apiKey: API_KEY, shopOrigin: shopOrigin };
+    const { Component, pageProps } = this.props;
+    const config = { apiKey: API_KEY, shopOrigin: Cookies.get("shopOrigin"), forceRedirect: true };
 
     return (
       <React.Fragment>
