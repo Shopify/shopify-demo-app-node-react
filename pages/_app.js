009ee421a01b7648b01bcd4d84ec633082620b52
Katie Davis <katie.davis@shopify.com>
2019-07-22 15:21:16 -0400

    save cookies

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import App from 'next/app';
 import Head from 'next/head';
 import { AppProvider } from '@shopify/polaris';
 import { Provider } from '@shopify/app-bridge-react';
+import Cookies from "js-cookie";
 import '@shopify/polaris/styles.css';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
@@ -20,8 +21,8 @@ class MyApp extends App {
   }
 
   render() {
-    const { Component, pageProps, shopOrigin } = this.props;
-    const config = { apiKey: API_KEY, shopOrigin: shopOrigin };
+    const { Component, pageProps } = this.props;
+    const config = { apiKey: API_KEY, shopOrigin: Cookies.get("shopOrigin"), forceRedirect: true };
 
     return (
       <React.Fragment>
