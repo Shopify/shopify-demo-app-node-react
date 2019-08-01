8b35a70135b78107b415fb2b1b20e4272d7cabb7
Katie Davis <katie.davis@shopify.com>
2019-08-01 13:12:03 -0400

    add dotenv (#42)

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
+require("dotenv").config();
 const withCSS = require('@zeit/next-css');
 const webpack = require('webpack');
 
 const apiKey = JSON.stringify(process.env.SHOPIFY_API_KEY);
+
 module.exports = withCSS({
   webpack: (config) => {
     const env = { API_KEY: apiKey };
-
     config.plugins.push(new webpack.DefinePlugin(env));
     return config;
   },
