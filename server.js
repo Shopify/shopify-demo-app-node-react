936efd25f485d22f6cc3010fce271301e27a174a
Katie Davis <katie.davis@shopify.com>
2020-02-04 11:51:22 -0500

    update for chrome 80

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY } = process.env;
 
 app.prepare().then(() => {
   const server = new Koa();
-  server.use(session(server));
+  server.use(session({ sameSite: 'none', secure: true }, server));
   server.keys = [SHOPIFY_API_SECRET_KEY];
 
   server.use(
