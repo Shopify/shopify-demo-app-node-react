fbfeda874784c401a535676c5ecacbe0b450c639
Aditya Narvekar <48922593+aditya5076@users.noreply.github.com>
2020-11-07 11:53:36 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import { EmptyState, Layout, Page } from '@shopify/polaris';
 import { ResourcePicker, TitleBar } from '@shopify/app-bridge-react';
 import store from 'store-js';
 import ResourceListWithProducts from '../components/ResourceList';
+import React from 'react';
+
 
 const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
